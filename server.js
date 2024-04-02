ba65df9dc15889dd7cb0622ace0b711b16db928d
Alex <chklff@gmail.com>
2024-04-02 13:16:09 -0400

    upload

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const ibmdb = require('ibm_db');
+const app = express();
+app.use(express.json());
+
+const dbConfig = `DATABASE=${process.env.DB_DATABASE};HOSTNAME=${process.env.DB_SERVER};UID=${process.env.DB_USER};PWD=${process.env.DB_PASSWORD};PORT=${process.env.DB_PORT};PROTOCOL=TCPIP`;
+
+app.post('/query', async (req, res) => {
+    ibmdb.open(dbConfig, function (err, conn) {
+        if (err) return res.status(500).send(err.message);
+        conn.query(req.body.sql, function (err, data) {
+            if (err) {
+                res.status(500).send(err.message);
+            } else {
+                res.json(data);
+            }
+            conn.close(function () {
+                console.log('Connection closed.');
+            });
+        });
+    });
+});
+
+app.get('/test-db', async (req, res) => {
+    ibmdb.open(dbConfig, function (err, conn) {
+        if (err) return res.status(500).send(err.message);
+        conn.query('SELECT * FROM SYSCAT.TABLES FETCH FIRST 10 ROWS ONLY', function (err, data) {
+            if (err) {
+                res.status(500).send(err.message);
+            } else {
+                res.json(data);
+            }
+            conn.close(function () {
+                console.log('Connection closed.');
+            });
+        });
+    });
+});
+
+const PORT = process.env.PORT || 5478;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
